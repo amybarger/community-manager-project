5b1fe775826c4540fa0d15c5862e0575660fa98d
amybarger <amy@care.ly>
2022-04-06 18:00:44 -0400

    Small adjustments to code and icon alignments on main page

--- pages/index.js
+++ pages/index.js
@@ -16,76 +16,81 @@ export default function Home() {
       <Nav />
       <main className={styles.main}>
         <div className={styles.textalignall}>
-        <Fade duration='1000'>
-        <img src="/BettyWhite.gif" alt="Gif of Betty White" className={styles.picsizes}></img>
-        <div className={styles.textalign}>
-        <h1 className={styles.title}>
-          Welcome to my page!
-        </h1>
+          <Fade duration='1000'>
+            <img src="/BettyWhite.gif" alt="Gif of Betty White" className={styles.picsizes}></img>
+           <div className={styles.textalign}>
+              <h1 className={styles.title}>
+                Welcome to my page!
+             </h1>
 
-        <p className={styles.description}>
-          Glad you’re here. Thanks for taking the time to check out my project! More below. :D
-        </p>
-        </div>
-        </Fade>
-        </div>
-
-        <div className={styles.textalignall}>
-        <Fade duration='1000'>
-        <img src="/Amy.jpg" alt="Picture of Amy Barger" className={styles.picsizes}></img>
-        <div className={styles.textalign}>
-        <h1 className={styles.title}>
-          Who did this stuff?
-        </h1>
-
-        <p className={styles.description}>
-          Amy Barger, Candidate for the Vercel Community Management role.
-        </p>
-        </div>
-        </Fade>
+              <p className={styles.description}>
+                Glad you’re here. Thanks for taking the time to check out my project!   More below. :D
+              </p>
+            </div>
+          </Fade>
         </div>
 
         <div className={styles.textalignall}>
-        <Fade duration='1000'>
-          <div className={styles.logosalign}>
-        <img src="/vercel_icon.png" alt="Logos of apps and languages" className={styles.logosizes}></img>
-        <img src="/nextjs_icon.png" alt="Logos of apps and languages" className={styles.logosizes}></img>
-        <img src="/3Logos.png" alt="Logos of apps and languages" className={styles.picsizes}></img>
-        </div>
-        <div className={styles.textalign}>
-        <h1 className={styles.title}>
-          Tools Used
-        </h1>
+          <Fade duration='1000'>
+            <img src="/Amy.jpg" alt="Picture of Amy Barger" className={styles.picsizes}></img>
+              <div className={styles.textalign}>
+                <h1 className={styles.title}>
+                  Who did this stuff?
+                </h1>
 
-        <p className={styles.description}>
-          This project was completed in Next.js and React.js frameworks.
-          <br></br>
-          <br></br>
-          Languages used: HTML, CSS, JavaScript.
-        </p>
-        </div>
-        </Fade>
+                <p className={styles.description}>
+                  Amy Barger, Candidate for the Vercel Community Management role.
+                </p>
+              </div>
+          </Fade>
         </div>
 
         <div className={styles.textalignall}>
-        <Fade duration='1000'>
-        <img src="/JanelleMonae.gif" alt="Gif of Janelle Monae" className={styles.picsizes}></img>
-        <div className={styles.textalign}>
-        <h1 className={styles.title}>
-          Answers to questions
-        </h1>
-
-        <p className={styles.description}>
-          First off — these questions were a fun challenge! You can <a href="/questions">click here</a> to a directory of the questions with my answers. These are also located in the nav bar at the top of this page. Enjoy!
-        </p>
+          <Fade duration='1000'>
+            <div className={styles.logosalign}>
+              <div className={styles.vercelnexticons}>
+              <div className={styles.vercelicon}>
+              <img src="/vercel_icon.png" alt="Logos of apps and languages" className={styles.logosizes}></img>
+           </div>
+              <img src="/nextjs_icon.png" alt="Logos of apps and languages" className={styles.logosizes}></img>
+              </div>
+              <div className={styles.languagesicons}>
+              <img src="/3Logos.png" alt="Logos of apps and languages" className={styles.picsizes}></img>
+              </div>
+            </div>
+            <div className={styles.textalign}>
+              <h1 className={styles.title}>
+              Tools Used
+              </h1>
+              <p className={styles.description}>
+              This project was completed in Next.js and React.js frameworks.
+              <br></br>
+              <br></br>
+              Languages used: HTML, CSS, JavaScript.
+              </p>
+            </div>
+          </Fade>
         </div>
-        </Fade>
+
+        <div className={styles.textalignall}>
+          <Fade duration='1000'>
+            <img src="/JanelleMonae.gif" alt="Gif of Janelle Monae" className={styles.picsizes}></img>
+            <div className={styles.textalign}>
+              <h1 className={styles.title}>
+                Answers to questions
+              </h1>
+
+              <p className={styles.description}>
+                First off — these questions were a fun challenge! You can <a href="/questions"><u>click here</u></a> to a directory of the questions with my answers. These are also located in the nav bar at the top of this page. Enjoy!
+              </p>
+            </div>
+          </Fade>
         </div>
 
       </main>
 
       <footer className={styles.footer}>
-      <a class="navbar-bran" href="https://vercel.com/about">
+        <a class="navbar-bran" href="https://vercel.com/about">
           <img src="/vercel_logo.png" alt="Vercel logo" className={styles.vercellogobottom}></img>
         </a>
         <p> Copyright © 2022 Vercel Inc. All rights reserved.</p>
