ed75f9a0dc263ccd087e88e9b00eb413be346b42
amybarger <amy@care.ly>
2022-04-06 15:18:44 -0400

    Styling Questions page and getting links working

--- pages/questions.js
+++ pages/questions.js
@@ -13,7 +13,29 @@ export default function All() {
             </Head>
 
             <Nav />
+            
+            <main className={styles.main}>
+            <div className={styles.questions}>
+            <h1 className={styles.title}>
+                Questions Directory
+            </h1>
+            <p className={styles.description}>
+                Click any question below for the answer.
+            </p>
 
+            <ul>
+                <li>
+                    <div className={styles.linkquestions}>
+                    <Link href="/article">
+                        
+                        <a >Which technical article, guide or lightning talk left the biggest positive impression on you? Why? Ideally, this article would not be associated with Vercel or web development, by the way! Please provide links.</a>
+                    </Link>
+                    </div>
+                </li>
+            </ul>
+            </div>
+            </main>
+            
             
         </div>
     )
