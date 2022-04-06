253dc9e6e7105f5d432ea2c8b1e8870fc203d0a3
amybarger <amy@care.ly>
2022-04-06 17:09:02 -0400

    Added content to community page

--- pages/community.js
+++ pages/community.js
@@ -14,6 +14,21 @@ export default function Community() {
 
             <Nav />
 
+            <main className={styles.main}>
+            <div className={styles.questions}>
+            <h1 className={styles.questionheader}>
+            What does Community mean to you?
+            </h1>
+            <p className={styles.answer}>
+            At its core essence, Community is survival and connection. Humans are super social creatures. We need Community to not only combat loneliness, but to understand ourselves, each other, and how we make sense of the world. I think our need for strong Communities has only grown since the COVID-19 pandemic — compassion, empathy, understanding, and connection are gifts us humans need to practice and embody every day. Community is also a way for us to share our talents and knowledge with each other to make us all stronger as a whole.
+            </p>
+            </div>
+            </main>
+            <footer className={styles.footer}>
+                <a class="navbar-bran" href="https://vercel.com/about">
+                <img src="/vercel_logo.png" alt="Vercel logo" className={styles.vercellogobottom}></img></a>
+                 <p> Copyright © 2022 Vercel Inc. All rights reserved.</p>
+            </footer>
             
         </div>
     )
