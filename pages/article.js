9ca340b55fd587e76da30a4f7e9f02c21c9b31d2
amybarger <amy@care.ly>
2022-04-06 16:27:02 -0400

    Creating article page

--- pages/article.js
+++ pages/article.js
@@ -14,7 +14,19 @@ export default function Article() {
 
             <Nav />
 
-            
+            <main className={styles.main}>
+            <div className={styles.questions}>
+            <h1 className={styles.questionheader}>
+            Which technical article, guide or lightning talk left the biggest positive impression on you? Why? Ideally, this article would not be associated with Vercel or web development, by the way! Please provide links. 
+            </h1>
+            <p className={styles.answer}>
+            Eric Ries’ article, <a href="https://www.fastcompany.com/1669738/to-get-to-the-root-of-a-hard-problem-just-ask-why-five-times"><u><i>To Get to the Root of a Hard Problem, Just Ask “Why” Five Times</i></u></a>, really resonated with me when I read it. It was a resource provided by the Google Data Analytics certification program. The article is a careful examination of why it’s so important to dig deeper and ask more meaningful questions when getting to the root of a problem. It reveals that the root solution is often followed by a series of issues that may otherwise not be apparent. I think this is a common issue in everyday business management. What we often thought was stitching up the problem was only a bandaid. This article helped drive-home my passion for more data-driven solutions to help a business and its community better connect and understand what’s really happening, not what we think is happening.
+            <br></br>
+            <br></br>
+            Also, some books I got recently that I can’t wait to read through: <a href="https://www.goodreads.com/book/show/34890015-factfulness"><u><i>Factfulness</i></u></a> by Hans Rosling; <a href="https://www.goodreads.com/en/book/show/58330567-atlas-of-the-heart"><u><i>Atlas of the Heart: Mapping Meaningful Connection and the Language of the Human Experience</i></u></a> by Brené Brown; and <a href="https://www.barnesandnoble.com/w/how-to-be-everything-emilie-wapnick/1124232447"><u><i>How to Be Everything</i></u></a> by Emilie Wapnick.
+            </p>
+            </div>
+            </main>
         </div>
     )
 }
