79d029f475ce336500d502f7dc0738a85ee183f2
amybarger <amy@care.ly>
2022-04-06 15:41:34 -0400

    Added all questions and their links to Questions page

--- pages/questions.js
+++ pages/questions.js
@@ -24,17 +24,74 @@ export default function All() {
             </p>
 
             <ul>
+            <div className={styles.linkquestions}>
                 <li>
-                    <div className={styles.linkquestions}>
+                    
                     <Link href="/article">
                         
-                        <a >Which technical article, guide or lightning talk left the biggest positive impression on you? Why? Ideally, this article would not be associated with Vercel or web development, by the way! Please provide links.</a>
+                        <a>Which technical article, guide or lightning talk left the biggest positive impression on you? Why? Ideally, this article would not be associated with Vercel or web development, by the way! Please provide links.</a>
                     </Link>
-                    </div>
                 </li>
+                <br></br>
+                <li>
+                    <Link href="/learn">
+                        <a>What do you want to learn, or do more of, at work?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/inapp-redirect">
+                        <a>Add a temporary in-app redirect to your project, redirecting /hello-vercel (or any path – but be sure to tell us which!) to https://vercel.com</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/redirect-response">
+                        <a>Imagine a customer reaches out in our GitHub community asking "How do I do a redirect from /hello-vercel to https://vercel.com?" In a couple of paragraphs, how do you respond?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/domain">
+                        <a>A customer writes in to our GitHub community stating "I have a custom domain which I purchased at GoDaddy and I want to use it on my project". In a couple of paragraphs, how do you respond?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/community">
+                        <a>What does Community mean to you?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/better-response">
+                        <a>Would, or how would, you improve the response shared with a customer here?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/common-problems">
+                        <a>What do you think is one of the most common problems which customers ask Vercel for help with? How would you help customers to overcome common problems, short-term and long-term?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/flagship">
+                        <a>If you could make one change to our flagship community, what would it be and why?</a>
+                    </Link>
+                </li>
+                <br></br>
+                <li>
+                    <Link href="/exercise">
+                        <a>How could we improve or alter this familiarisation exercise?</a>
+                    </Link>
+                </li>
+                
+                </div>
             </ul>
             </div>
             </main>
+            
             
             
         </div>
