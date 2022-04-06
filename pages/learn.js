00ba92f1e8b17eb58d21c14bbfbb295d77f85e66
amybarger <amy@care.ly>
2022-04-06 16:34:02 -0400

    Created Learn page

--- pages/learn.js
+++ pages/learn.js
@@ -14,6 +14,21 @@ export default function Learn() {
 
             <Nav />
 
+            <main className={styles.main}>
+            <div className={styles.questions}>
+            <h1 className={styles.questionheader}>
+            What do you want to learn, or do more of, at work?
+            </h1>
+            <p className={styles.answer}>
+            I’d love to do more projects that require a deeper-dive investigation to come up with really cool campaigns or solutions that apply the majority of my skill sets and passions. What’s exciting about the Community Manager role at Vercel is how I could help build a stronger sense of community for Vercel users, provide them solutions with my knowledge of code and growing understanding of Vercel and its products, and also my data analysis skills to understand and tackle current challenges for the users. I love a role that doesn’t keep me in a super tight box and allows me to expand in many ways!
+            </p>
+            </div>
+            </main>
+            <footer className={styles.footer}>
+                <a class="navbar-bran" href="https://vercel.com/about">
+                <img src="/vercel_logo.png" alt="Vercel logo" className={styles.vercellogobottom}></img></a>
+                 <p> Copyright © 2022 Vercel Inc. All rights reserved.</p>
+            </footer>
             
         </div>
     )
