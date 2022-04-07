3b2d2b59b104916f3e0c3bfbe8f283b0b9b1e838
amybarger <amy@care.ly>
2022-04-06 21:21:40 -0400

    Disabling nav component

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,74 +1,74 @@
-import styles from '../styles/Home.module.css'
-import { useEffect, useState } from "react";
+// import styles from '../styles/Home.module.css'
+// import { useEffect, useState } from "react";
 
-export default function NavBar() {
+// export default function NavBar() {
   
-  const [clientWindowHeight, setClientWindowHeight] = useState("");
+//   const [clientWindowHeight, setClientWindowHeight] = useState("");
 
-  const [backgroundTransparacy, setBackgroundTransparacy] = useState(0);
-  const [padding, setPadding] = useState(30);
-  const [boxShadow, setBoxShadow] = useState(0);
+//   const [backgroundTransparacy, setBackgroundTransparacy] = useState(0);
+//   const [padding, setPadding] = useState(30);
+//   const [boxShadow, setBoxShadow] = useState(0);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  });
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   });
 
-  const handleScroll = () => {
-    setClientWindowHeight(window.scrollY);
-  };
+//   const handleScroll = () => {
+//     setClientWindowHeight(window.scrollY);
+//   };
 
-  useEffect(() => {
-    let backgroundTransparacyVar = clientWindowHeight / 600;
+//   useEffect(() => {
+//     let backgroundTransparacyVar = clientWindowHeight / 600;
 
-    if (backgroundTransparacyVar < .5) {
-      let paddingVar = 30 - backgroundTransparacyVar * 20;
-      let boxShadowVar = backgroundTransparacyVar * 15;
-      setBackgroundTransparacy(backgroundTransparacyVar);
-      setPadding(paddingVar);
-      setBoxShadow(boxShadowVar);
-    }
-  }, [clientWindowHeight]);
+//     if (backgroundTransparacyVar < .5) {
+//       let paddingVar = 30 - backgroundTransparacyVar * 20;
+//       let boxShadowVar = backgroundTransparacyVar * 15;
+//       setBackgroundTransparacy(backgroundTransparacyVar);
+//       setPadding(paddingVar);
+//       setBoxShadow(boxShadowVar);
+//     }
+//   }, [clientWindowHeight]);
 
-  return (
-    <nav
-      class="navbar navbar-expand-lg navbar-light fixed-top"
-      style={{
-        background: `rgba(0, 0, 0, ${backgroundTransparacy})`,
-        padding: `${padding}px 0px`,
-        boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 20px 6px`,
-      }}
-    >
-      <div className={styles.navbar}>
-        <a class="navbar-bran" href="https://vercel.com/about">
-          <img src="/vercel_logo.png" alt="Vercel logo" className={styles.vercellogo}></img>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse-navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class={styles.navlink} Link href="/">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class={styles.navlink} href="/questions">
-                Questions
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-}
+//   return (
+//     <nav
+//       class="navbar navbar-expand-lg navbar-light fixed-top"
+//       style={{
+//         background: `rgba(0, 0, 0, ${backgroundTransparacy})`,
+//         padding: `${padding}px 0px`,
+//         boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 20px 6px`,
+//       }}
+//     >
+//       <div className={styles.navbar}>
+//         <a class="navbar-bran" href="https://vercel.com/about">
+//           <img src="/vercel_logo.png" alt="Vercel logo" className={styles.vercellogo}></img>
+//         </a>
+//         <button
+//           class="navbar-toggler"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarText"
+//           aria-controls="navbarText"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <div class="collapse-navbar-collapse" id="navbarText">
+//           <ul class="navbar-nav mr-auto">
+//             <li class="nav-item active">
+//               <a class={styles.navlink} Link href="/">
+//                 Home
+//               </a>
+//             </li>
+//             <li class="nav-item">
+//               <a class={styles.navlink} href="/questions">
+//                 Questions
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
